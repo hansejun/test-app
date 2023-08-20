40b8a5a37fd5ba74cc0b050d498c2150b9450487
hansejun <86880916+hansejun@users.noreply.github.com>
2023-08-21 00:07:26 +0900

    Update index.test.tsx

--- src/chapter/2/index.test.tsx
+++ src/chapter/2/index.test.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+
 import ButtonSection from '.';
 
-test('Button has correct initial color', () => {
+test('Button has correct initial color and updates click', () => {
   render(<ButtonSection />);
   // Change to blue라는 텍스트를 표시하는 요소 찾기
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
@@ -10,8 +11,14 @@ test('Button has correct initial color', () => {
   expect(colorButton).toHaveStyle({
     backgroundColor: 'rgb(239 68 68 / 1)',
   });
-});
 
-test('Button turns blue when clicked ', () => {
-  render(<ButtonSection />);
+  // click button
+  fireEvent.click(colorButton);
+
+  // 클릭하고 난 뒤에 색상이 해당 색상이 맞는지 확인
+    backgroundColor: 'rgb(59 130 246 / 1)',
+  });
+
+  // 클릭하고 난 뒤에 텍스트가 'Change to red'가 맞는지 확인
+  expect(colorButton).toHaveTextContent('Change to red');
 });
