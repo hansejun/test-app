ed74f462d3957b9c7963651b546b158e634052c9
hansejun <86880916+hansejun@users.noreply.github.com>
2023-08-21 00:31:28 +0900

    체크 박스 테스트

--- src/chapter/2/index.test.tsx
+++ src/chapter/2/index.test.tsx
@@ -16,9 +16,30 @@ test('Button has correct initial color and updates click', () => {
   fireEvent.click(colorButton);
 
   // 클릭하고 난 뒤에 색상이 해당 색상이 맞는지 확인
+  expect(colorButton).toHaveStyle({
     backgroundColor: 'rgb(59 130 246 / 1)',
   });
 
   // 클릭하고 난 뒤에 텍스트가 'Change to red'가 맞는지 확인
   expect(colorButton).toHaveTextContent('Change to red');
+});
+
+test('체크 박스 체크 테스트', () => {
+  render(<ButtonSection />);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorButton).toBeEnabled();
+
+  const checkBox = screen.getByRole('checkbox');
+  expect(checkBox).not.toBeChecked();
+
+  fireEvent.click(checkBox);
+
+  expect(checkBox).toBeChecked();
+  expect(colorButton).toBeDisabled();
+
+  fireEvent.click(checkBox);
+
+  expect(colorButton).toBeEnabled();
+  expect(checkBox).not.toBeChecked();
 });
--- src/chapter/2/index.tsx
+++ src/chapter/2/index.tsx
@@ -2,14 +2,22 @@ import React, { useState } from 'react';
 
 const ButtonSection = () => {
   const [isBlue, setIsBlue] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
 
   const color = isBlue ? 'bg-blue-500' : 'bg-red-500';
   const text = isBlue ? 'Change to red' : 'Change to blue';
 
   return (
-    <button className={`${color} p-4`} onClick={() => setIsBlue(prev => !prev)}>
-      {text}{' '}
-    </button>
+    <div>
+      <button
+        className={`${color} p-4`}
+        disabled={isChecked}
+        onClick={() => setIsBlue(prev => !prev)}
+      >
+        {text}{' '}
+      </button>
+      <input type="checkbox" onChange={e => setIsChecked(e.target.checked)} />
+    </div>
   );
 };
 
